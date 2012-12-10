a80a098205d960ba3681596cf50cd894e7a4954f
Nick Sillik <nsillik@squareup.com>
2012-12-10 11:20:41 -0800

    Initial Commit

---
+++ OpenCV/OpenCV.h
@@ -0,0 +1,13 @@
+//
+//  OpenCV.h
+//  OpenCV
+//
+//  Created by Nick Sillik on 12/10/12.
+//  Copyright (c) 2012 Sillik.org. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OpenCV : NSObject
+
+@end
---
+++ OpenCV/OpenCV.m
@@ -0,0 +1,13 @@
+//
+//  OpenCV.m
+//  OpenCV
+//
+//  Created by Nick Sillik on 12/10/12.
+//  Copyright (c) 2012 Sillik.org. All rights reserved.
+//
+
+#import "OpenCV.h"
+
+@implementation OpenCV
+
+@end
